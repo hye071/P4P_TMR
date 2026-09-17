7bf92ed991ced42405f2b8ed81c36887d74366d4
hye071 <hye071@aucklanduni.ac.nz>
2026-09-17 22:49:21 +1200

    saving work

--- Firmware/MATLAB/Plotting_SWV_data_trace/plotting_swv.m
+++ Firmware/MATLAB/Plotting_SWV_data_trace/plotting_swv.m
@@ -21,7 +21,7 @@
 clear; clc; close all;
 
 %% ---- User setting ----
-filename = '0A_NS3_INA.txt';   % <-- change this if the file lives elsewhere
+filename = '-502mA_NSR.txt';   % <-- change this if the file lives elsewhere
 
 %% ---- Read the file as raw bytes ----
 % The export is ISO-8859-1 (Latin-1) encoded -- that's the encoding behind
