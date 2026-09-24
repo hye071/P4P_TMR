7f00e4b62a1e7172ac1817a8e0e64518b573a896
hye071 <hye071@aucklanduni.ac.nz>
2026-09-24 18:39:40 +1200

    collected data for sheilding for non-radited board

--- Firmware/MATLAB/Plotting_SWV_data_trace/plotting_swv.m
+++ Firmware/MATLAB/Plotting_SWV_data_trace/plotting_swv.m
@@ -21,7 +21,7 @@
 clear; clc; close all;
 
 %% ---- User setting ----
-filename = '-2999mA_NSR_INA.txt';   % <-- change this if the file lives elsewhere
+filename = '-3000mA_S_INA.txt';   % <-- change this if the file lives elsewhere
 
 %% ---- Read the file as raw bytes ----
 % The export is ISO-8859-1 (Latin-1) encoded -- that's the encoding behind
